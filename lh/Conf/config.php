ee346ec0ab25b30b859831626de60ff5f82eecd4
luxiangrong <luxiangrongwave@gmail.com>
2015-01-14 12:30:41 +0800

    remove the attachment upload button

--- lh/Conf/config.php
+++ lh/Conf/config.php
@@ -43,8 +43,8 @@ return array(
 	
 				
 	//配置上传图片的一些配置
-	'XSIZE' => 10485760, 							//设置附件上传大小
-	'ALLOWEXTS'=>array('jpg', 'gif', 'png', 'jpeg','rar','doc','docx'),//设置附件上传类型
+	'XSIZE' => 30485760, 							//设置附件上传大小
+	'ALLOWEXTS'=>array('jpg', 'gif', 'png', 'jpeg','rar','doc','docx','pdf'),//设置附件上传类型
 	'SAVEPATH' => './Public/uploads/',				// 设置附件上传目录
 	
 );
